81554871a144c0339fefaf746aeaf0484bdd0d4d
Ivan Sysoev <vanysysoev@mail.ru>
2024-12-07 09:32:11 +0300

    Adding impl

--- seminar3/src/main.rs
+++ seminar3/src/main.rs
@@ -14,7 +14,13 @@ struct ApartmentPrice{
 
 impl ApartmentPrice{
     fn is_too_high(&self) -> bool{
-        self.price > 150000.0
+        self.price >= 150000.0
+    }
+    fn smth() -> i32{
+        150
+    }
+    fn change_price(&mut self, delta: f32){
+        self.price += delta
     }
 }
 
@@ -34,18 +40,38 @@ struct Apartment {
 
 // type ApartmentPrice = f32; or we can use it as 
 
+trait HighPriceTrait{
+    fn is_too_high(&self) -> bool{
+        unimplemented!()
+    }
+}
+
+impl HighPriceTrait for ApartmentPrice{
+    fn is_too_high(&self) -> bool{
+        self.price >=200000.0
+    }
+}
 
 fn main(){
-    let apartment = Apartment {
+    let mut apartment = Apartment {
         room_areas: [15;3],
         geolocation: Coord{
             latitude: 55.762,
             longtitude: 33.617,
         },
 
-        price_data: ApartmentPrice{price: 152000.0, currency: String::from("RUB")},
+        price_data: ApartmentPrice{price: 149000.0, currency: String::from("RUB")},
     };
     println!("{:?}",apartment);
     //apartment.total_price.f();
     println!("{:?}",apartment.price_data.is_too_high());
+    println!("{:?}",ApartmentPrice::smth());
+    println!("{:?}",apartment.price_data.change_price(1000.0));
+    println!("{:?}",apartment.price_data.is_too_high());
+
+    println!("{:?}",apartment);
+
+    println!("{:?}", HighPriceTrait::is_too_high(&apartment.price_data)); // or
+    println!("{:?}", <ApartmentPrice as HighPriceTrait>::is_too_high(&apartment.price_data));
+
 }
